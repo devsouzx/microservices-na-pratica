dceca9b071acf656b3f8e994a44999075be56042
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-09-30 18:58:51 -0300

    first commit

---
+++ src/main/java/com/devsouzx/microservicos_na_pratica/MicroservicosNaPraticaApplication.java
@@ -0,0 +1,13 @@
+package com.devsouzx.microservicos_na_pratica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicosNaPraticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicosNaPraticaApplication.class, args);
+	}
+
+}
